d5c4b31389fa5cbe015b767072346605db6552ee
Suleyman Sinan Yasak <118135597+ssyasak@users.noreply.github.com>
2023-04-04 20:32:58 +0300

    Update erinc.py

--- klimatoloji/erinc.py
+++ klimatoloji/erinc.py
@@ -1,1 +1,30 @@
+# Erinç İklim Sınıflandırması
 
+# Aşağıda istenilen değerleri giriniz:
+
+P = float(input("Yıllık Toplam Yağış Miktarı (mm): "))
+Tom = float(input("Yıllık Ortalama Maksimum Sıcaklık Değeri (°C): "))
+
+# Yağış Etkinlik İndeksi aşağıdaki formül ile bulunur.
+
+Im = P / Tom
+
+print("Yağış Etkinlik İndeksi: " + str(Im))
+
+# Bulunan indeks değeri aşağıdaki şekilde sınıflandırılmıştır.
+print("İklim Tipi:")
+
+if Im < 8:
+    print("Tam Kurak")
+elif 8 < Im < 15:
+    print("Kurak")
+elif 15 < Im < 23:
+    print("Yarı Kurak")
+elif 23 < Im < 40:
+    print("Yarı Nemli")
+elif 40 < Im < 55:
+    print("Nemli")
+else:
+    print("Çok Nemli")
+
+# Kaynak: Erinç, S. (1984). Klimatoloji ve Metotları, İ.T.Ü. Deniz Bilimleri ve Coğrafya Enstitüsü, İstanbul.
